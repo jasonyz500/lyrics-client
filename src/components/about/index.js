7113e37df2a2eb09e659ccb6bb8406a49fdca60c
Jason Zhou <jasonzhou@jasons-mbp.lan>
2024-08-03 22:02:28 -0700

    asdf

--- src/components/about/index.js
+++ src/components/about/index.js
@@ -8,7 +8,7 @@ function About() {
         <p>I'm a programmer, a Japanese music enthusiast and hobby translator. I built this site to practice web development and share my translations.</p>
         <p>I try to make my translations as expressive as possible, while not deviating from the literal meaning of the original. The goal is a translation that could be quality English song lyrics (but without the rhyme).</p>
         <p>Sometimes I'll re-translate something that already exists on the internet but with some grammatical mistakes or translation mistakes. I'll credit them when I do.</p>
-        <Box display="flex">Find me on <Text display="inline" color="shopping"><Link display="inline" href='https://www.youtube.com/channel/UC0xE-0jjE0IBAxhozD4Hjfg'>Youtube</Link></Text> and <Text inline color="shopping"><Link inline href='https://lyricstranslate.com/en/translator/jasonyz500'>LyricsTranslate</Link></Text>.</Box>
+        <Box display="flex">Find me on <Text display="inline" color="shopping"><Link display="inline" href='https://www.youtube.com/channel/UC0xE-0jjE0IBAxhozD4Hjfg'>Youtube</Link></Text></Box>
       </Text>
     </Box>
   );
